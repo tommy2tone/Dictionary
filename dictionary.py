21d59153e988225ca9fbf094543632bcd4116779
tommy2tone <thomas.hildebrand1@gmail.com>
2019-08-01 22:35:21 -0400

    Placed conditional statements in a function called "lookup" in order to utilize recursion in the event the user wanted to enter a new word.

--- dictionary.py
+++ dictionary.py
@@ -5,14 +5,35 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 # Request word from user
-word = input('Please enter a word. ')
+word = input('Please enter a word: ')
 
 # Test conditions
-if word in data:
-    print(data[word])
-elif len(get_close_matches(word, data, cutoff=0.8)) > 0:
-    closet_match = get_close_matches(word, data)[0]
-    print(f'Did you mean {closet_match}?')
-else:
-    print('The word entered does not exist.  Please try again.')
+
+
+def lookup(word):
+    if word in data:
+        print(data[word])
+    elif len(get_close_matches(word, data, cutoff=0.8)) > 0:
+        closet_match = get_close_matches(word, data)[0]
+
+    # Ask user if they meant the closet match
+        choice = input(f'Did you mean {closet_match}?  Press Y for Yes or N for No: ')
+        if choice.lower() == 'y':
+            print(data[closet_match])
+        else:
+            word = input('Please enter a word or press Q to quit: ')
+            if word.lower() != 'q':
+                lookup(word)
+
+    else:
+        word = input('The word entered does not exist.  Please try again or Press Q to quit.')
+        if word.lower() != 'q':
+            lookup(word)
+
+
+
+lookup(word)
+
+
+
 
